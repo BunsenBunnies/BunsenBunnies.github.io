d0ea804c7435eda89676e6c9cfc8a4a0e78cdd0e
Dragon Harrison <dragon@Anns-MacBook-Air-2.local>
2019-08-12 14:11:30 +0100

    gulp, scss, and updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
 
 // Static server
 gulp.task('browser-sync', function() {
@@ -8,4 +9,16 @@ gulp.task('browser-sync', function() {
             baseDir: "./"
         }
     });
+    
+    gulp.watch("**/*.html").on('change', browserSync.reload);
+
 });
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("scss/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("css"))
+        .pipe(browserSync.stream());
+});
+
