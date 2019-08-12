79973ebced0aad4cf116458946081e4b0cfb653d
Dragon Harrison <dragon@Anns-MacBook-Air-2.local>
2019-08-12 15:53:46 +0100

    Image upload

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,6 @@ var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
 
-// Static server
-gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        }
-    });
-    
-    gulp.watch("**/*.html").on('change', browserSync.reload);
-
-});
-
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
     return gulp.src("scss/master.scss")
@@ -22,3 +10,18 @@ gulp.task('sass', function() {
         .pipe(browserSync.stream());
 });
 
+// Static server
+gulp.task('browser-sync',gulp.series('sass',function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    
+    gulp.watch("scss/*.scss", gulp.parallel('sass'));
+    gulp.watch("**/*.html").on('change', browserSync.reload);
+
+
+}));
+
+gulp.task('default', gulp.parallel ('browser-sync'));
